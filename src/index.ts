5ca3b9d5b577a430c786484dcde4fdd0f73b0c99
Collei <collei@collei.com.br>
2025-12-31 00:20:40 -0300

    added index

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as Dates from "./functions/dates";
+import * as Compares from "./functions/compares";
+
+/**
+ * exports
+ */
+
+export const dates = Dates.dates;
+export const compares = Compares.compares;
